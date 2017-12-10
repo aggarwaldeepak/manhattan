e2e2452c2681799aaf9c25de2bc664007ad97587
Anirudh Gupta <firefinchdev@gmail.com>
2017-12-10 23:39:38 +0530

    Add: Layout Build
    * Skeleton Setup
    
    Signed-off-by: Anirudh Gupta <firefinchdev@gmail.com>
    
    * ..
    
    * Bottom Navigation with Labels Addedd
    
    * Menu added
    
    * Menu button
    
    * ...
    
    Signed-off-by: Anirudh Gupta <firefinchdev@gmail.com>
    
    * Hindi support added

---
+++ app/src/main/java/com/agarwal/vinod/govindkigali/MainActivity.java
@@ -0,0 +1,122 @@
+package com.agarwal.vinod.govindkigali;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.agarwal.vinod.govindkigali.Utils.BottomNavigationViewHelper;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+    private Toolbar toolbar;
+    SearchView searchView;
+
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_play:
+                    mTextMessage.setText(R.string.title_play);
+                    return true;
+                case R.id.navigation_thought:
+                    mTextMessage.setText(R.string.title_thought);
+                    return true;
+                case R.id.navigation_upcoming:
+                    mTextMessage.setText(R.string.title_upcoming);
+                    return true;
+                case R.id.navigation_my_music:
+                    mTextMessage.setText(R.string.title_my_music);
+                    return true;
+                case R.id.navigation_settings:
+                    mTextMessage.setText(R.string.title_settings);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTextMessage = (TextView) findViewById(R.id.message);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        BottomNavigationViewHelper.disableShiftMode(navigation);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        searchView = (SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.action_search));
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Log.d("Yooo", "onQueryTextSubmit: " + query);
+                searchView.clearFocus();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Log.d("Yooo", "onQueryTextSubmit: " + newText);
+                return false;
+            }
+
+
+        });
+
+        searchView.setOnCloseListener(new SearchView.OnCloseListener() {
+            @Override
+            public boolean onClose() {
+                return false;
+            }
+        });
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_notifications:
+                break;
+            case R.id.action_search:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (!searchView.isIconified()) {
+            searchView.setIconified(true);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+
+
+
+}
