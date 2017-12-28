e7c5165e21faedbc1b6e94ac9b5e82f81fcac928
Anirudh Gupta <anirudhgupta281998@gmail.com>
2017-12-28 14:22:26 +0530

    Update: Thought

--- app/src/main/java/com/agarwal/vinod/govindkigali/adapters/ThoughtAdapter.java
+++ app/src/main/java/com/agarwal/vinod/govindkigali/adapters/ThoughtAdapter.java
@@ -1,17 +1,22 @@
 package com.agarwal.vinod.govindkigali.adapters;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.support.annotation.NonNull;
 import android.support.v4.view.PagerAdapter;
+import android.support.v7.widget.AppCompatImageView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.agarwal.vinod.govindkigali.R;
+import com.agarwal.vinod.govindkigali.models.Thought;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -29,7 +34,20 @@ public class ThoughtAdapter extends PagerAdapter {
     private LayoutInflater layoutInflater;
     Context context;
     DatabaseReference databaseReference;
-    ArrayList<String> thoughtList;
+    ArrayList<Thought> thoughtList;
+    public static final int BLACK = android.R.color.black;
+    public static final int WHITE = android.R.color.white;
+    int[] backgroundColor = {
+            Color.parseColor("#b6e1ab"),
+            Color.parseColor("#e1ddab"),
+            Color.parseColor("#e1bfab")
+    };
+
+    int[] textColor = {
+            WHITE,
+            WHITE,
+            WHITE
+    };
 
     public ThoughtAdapter(Context context) {
         this.context = context;
@@ -39,10 +57,8 @@ public class ThoughtAdapter extends PagerAdapter {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 for (DataSnapshot dataSnap : dataSnapshot.getChildren()){
-
-                    //TODO: Check it not working force closing APP
-//                    Log.d("THOUGHT", "onDataChange: " + dataSnap.getValue(String.class));
-//                    thoughtList.add(dataSnap.getValue(String.class));
+                    Log.d("THOUGHT", "onDataChange: " + dataSnap.getValue(Thought.class));
+                    thoughtList.add(dataSnap.getValue(Thought.class));
                     notifyDataSetChanged();
                 }
             }
@@ -67,21 +83,29 @@ public class ThoughtAdapter extends PagerAdapter {
 
         View view = layoutInflater.inflate(R.layout.layout_thought, container, false);
         TextView tvThoughtText = view.findViewById(R.id.tv_thought_text);
-        tvThoughtText.setText(thoughtList.get(position));
-        final LinearLayout linearLayout = view.findViewById(R.id.rootBottomLayout);
-
+        tvThoughtText.setText(thoughtList.get(position).getText());
+        tvThoughtText.setTextColor(context.getResources().getColor(textColor[position % textColor.length]));
+        AppCompatImageView imageView = view.findViewById(R.id.ivBackground);
+       // final LinearLayout linearLayout = view.findViewById(R.id.rootBottomLayout);
+        imageView.setColorFilter(backgroundColor[position % backgroundColor.length], PorterDuff.Mode.MULTIPLY);
         FrameLayout frameLayout = view.findViewById(R.id.rootLayoutThought);
+
+        ImageView header = view.findViewById(R.id.iv_header);
+        ImageView footer = view.findViewById(R.id.iv_footer);
+
+        header.setColorFilter(backgroundColor[position % backgroundColor.length], PorterDuff.Mode.SRC_IN);
+        footer.setColorFilter(backgroundColor[position % backgroundColor.length], PorterDuff.Mode.SRC_IN);
         frameLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                switch (linearLayout.getVisibility()){
+               /* switch (linearLayout.getVisibility()){
                     case View.GONE:
                         linearLayout.setVisibility(View.VISIBLE);
                         break;
                     case View.VISIBLE:
                         linearLayout.setVisibility(View.GONE);
                         break;
-                }
+                }*/
             }
         });
         container.addView(view);
